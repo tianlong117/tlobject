f17a0309f9649f4045fd2f91e00e26df2cda8ae0
天龙 <tianlong117@qq.com>
2021-12-29 16:52:17 +0800

    添加jettyserver

---
+++ demo/web/src/main/java/cn/tianlong/java/jettyserver/serverstart.java
@@ -0,0 +1,30 @@
+package cn.tianlong.java.jettyserver;
+
+import cn.tianlong.tlobject.base.TLObjectFactory;
+import cn.tianlong.tlobject.modules.TLAppStartUp;
+
+/**
+ * 创建日期：2021/2/1010:29
+ * 描述:
+ * 作者:tianlong
+ */
+public class serverstart extends TLAppStartUp {
+    public static TLObjectFactory appFactory ;
+    public serverstart(String name) {
+        super( name);
+    }
+    public static void  main (String[] args ) {
+        startModule (args );
+    }
+    @Override
+    protected void run() {
+  //      moduleFactory.shutdown(1);
+  //      putMsg("wife@startup1",createMsg().setAction("sing"));
+    }
+    public static TLObjectFactory startModule (String[] args  ) {
+        String[] appArgs = {CLASSPATH+"/conf/jettyserver/","jettyserver_factory_config.xml",null,"serverstart"};
+        serverstart instance = new serverstart("serverstart");
+        appFactory=  instance.startup(appArgs);
+        return appFactory ;
+    }
+}
