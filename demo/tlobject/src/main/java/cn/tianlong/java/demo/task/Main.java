9038dec13c81ed34d0d6613e70839ccc8a81ae7e
天龙 <tianlong117@qq.com>
2026-08-17 12:31:02 +0800

    8月15日 ，拆分agent ，增加trace功能

--- demo/tlobject/src/main/java/cn/tianlong/java/demo/task/Main.java
+++ demo/tlobject/src/main/java/cn/tianlong/java/demo/task/Main.java
@@ -1,20 +1,26 @@
 package cn.tianlong.java.demo.task;
-import cn.tianlong.tlobject.base.TLMsg;
-import cn.tianlong.tlobject.base.TLObjectFactory;
+import cn.tianlong.tlobject.modules.TLAppStartUp;
+import java.util.HashMap;
 
-public class Main  {
+/**
+ * 创建日期：2021/2/1010:29
+ * 描述:
+ * 作者:tianlong
+ */
+public class Main extends TLAppStartUp {
 
-    public static void main(String[] args)
-    {
-        String configdir = "/conf/demo/task/";
-        String factoryConfigPath =Main.class.getResource(configdir).getPath();
-        System.setProperty("log4j.configurationFile", factoryConfigPath+"log4j2.xml");
-        TLObjectFactory myfactory = TLObjectFactory.getInstance( factoryConfigPath,"moduleFactory_config.xml");
-        myfactory.startFactory(null,null);
-        myfactory.boot();
-   //    TLMsg taksMsg =new TLMsg().setAction("getModule").setParam("moduleName","myTaskManger");
-     //   myfactory.putMsg(myfactory,taksMsg);
-    //    myfactory.putMsg("mymsgTaskConsole",new TLMsg().setAction("startScan"));
+    public static void  main (String[] args ) {
+        if(args !=null && args.length >0){
+            TLAppStartUp.main(args);
+            return;
+        }
+        HashMap<String,String> argsMap =new HashMap<>() ;
+        argsMap.put("appName","demo0");
+        argsMap.put("configPath",CLASSPATH+"/conf/demo/task/");
+        argsMap.put("factoryConfigFile","moduleFactory_config.xml");
+        argsMap.put("configFile","");
+        TLAppStartUp.main0(argsMap);
+        TLAppStartUp.shutdown();
+    }
 
-}
 }
